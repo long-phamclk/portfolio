52a86c1404435bce7387fdf6b7535d7a5f0cc44e
chilikilong112 <chiliki.long@gmail.com>
2023-08-08 13:29:59 +1000

    add loading effect

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,26 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  // const [load, upadateLoad] = useState(true);
+  const [load, setLoad] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     upadateLoad(false);
-  //   }, 1200);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 1200);
 
-  //   return () => clearTimeout(timer);
-  // }, []);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <>
-      <Router>
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
@@ -35,8 +40,9 @@ function App() {
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </Router>
-    </>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
